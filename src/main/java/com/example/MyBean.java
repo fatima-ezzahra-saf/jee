dacd69494915933333b93ca45ede2b12856ed681
fatima_ezzahra <fatimaezzahrasafrani@gmail.com>
2024-10-07 10:19:22 +0000

    login

--- src/main/java/com/example/MyBean.java
+++ src/main/java/com/example/MyBean.java
@@ -7,22 +7,38 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class MyBean {
     private String name;
+    private String password;
     private String message;
 
-    public void submit() {
-        message = "Bonjour, " + name + "!";
+
+    public String submit() {
+        if(name.equals("fati") && password.equals("fati123")) return "nextPage?faces-redirect=true";
+        else {
+           setMessage("name ou mot de passe incorrect");
+            return message;
+        }
     }
 
-    // Getters et setters
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     public String getName() {
         return name;
+    }
+    public String getPassword(){
+        return password;
     }
 
     public void setName(String name) {
         this.name = name;
     }
+    public void setPassword(String password){this.password=password;}
 
     public String getMessage() {
         return message;
+    }
+    public String nextPage(){
+        return "nextPage?faces-redirect=true";
     }
 }
