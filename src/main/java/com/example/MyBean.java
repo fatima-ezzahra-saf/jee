db645ca2a01f265f02855ec8223e3141123c26ed
fatima_ezzahra <fatimaezzahrasafrani@gmail.com>
2024-10-21 08:00:21 +0000

    Login & redirection

--- src/main/java/com/example/MyBean.java
+++ src/main/java/com/example/MyBean.java
@@ -2,43 +2,52 @@ package com.example;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 @RequestScoped
 public class MyBean {
-    private String name;
     private String password;
+    private String email;
     private String message;
 
-
     public String submit() {
-        if(name.equals("fati") && password.equals("fati123")) return "nextPage?faces-redirect=true";
-        else {
-           setMessage("name ou mot de passe incorrect");
-            return message;
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        if (email.equals("fati@gmail.com") && password.equals("fati123")) {
+            return "nextPage?faces-redirect=true";
+        } else {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Nom ou mot de passe incorrect", null));
+            return null; // Stay on the same page
         }
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    // Getters and Setters
 
-    public String getName() {
-        return name;
-    }
-    public String getPassword(){
+
+    public String getPassword() {
         return password;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPassword(String password) {
+        this.password = password;
     }
-    public void setPassword(String password){this.password=password;}
+
+    public String getEmail() { // Add getter for email
+        return email;
+    }
+
+    public void setEmail(String email) { // Add setter for email
+        this.email = email;
+    }
 
     public String getMessage() {
         return message;
     }
-    public String nextPage(){
-        return "nextPage?faces-redirect=true";
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
